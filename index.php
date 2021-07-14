d658122964ff9efe90f3edcfaeaa861af49fb7e2
Sai Eshwar Reddy Kottapally <76461247+eshwar3214@users.noreply.github.com>
2021-07-14 14:59:12 +0530

    added footer

--- index.php
+++ index.php
@@ -57,6 +57,54 @@
 
    
       <br>
+    <footer class="bg-dark text-center text-white">
+  <br><br>
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <p align="center">explore me on social media</p>
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/sai.eshwar.3990/" role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+  
+        <!-- Twitter -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/EshwarKothapal1?s=08" role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+  
+        <!-- Google -->
+        <a class="btn btn-outline-light btn-floating m-1" href="contact.html" role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+  
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/uceou_trolls/ " role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+  
+        <!-- Linkedin -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/sai-eshwar-reddy-306a22197/" role="button"
+          ><i class="fab fa-linkedin-in"></i
+        ></a>
+  
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/eshwar3214/" role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    <br><br>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © Copyright: uceou_trolls
+    </div>
+    <!-- Copyright -->
+  </footer>
       
     
     
