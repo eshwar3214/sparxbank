e58ddfdff60ea7cc84571cd4449b9cf7842ff357
Sai Eshwar Reddy Kottapally <76461247+eshwar3214@users.noreply.github.com>
2021-07-14 15:47:48 +0530

    added navabar to details.php

--- details.php
+++ details.php
@@ -13,8 +13,33 @@
   
   <body>
     
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     
-    <h1 align='center'>customer details</h1>
+    
+        <a class="navbar-brand" href="index.php">sparx bank</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="details.php">view customers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transferslist.php">previous transfers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </nav>
+    
+    
+    <h3 align='center'>customer details</h1>
     <?php
  
     $username='sql5425083';
