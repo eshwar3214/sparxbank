db4a4422e94d0455368db883c5fd15860b098bce
Sai Eshwar Reddy Kottapally <76461247+eshwar3214@users.noreply.github.com>
2021-07-14 16:31:27 +0530

    changed index.php

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>sparx bank</title>
-    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>explore.contact.html</title>
+    <link rel="icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="style.css">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    
-<body>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/be839c75d0.js" crossorigin="anonymous"></script>
+</head>
+<body style="background-color: cornsilk;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     
     
         <a class="navbar-brand" href="index.php">sparx bank</a>
@@ -36,41 +38,20 @@
         </ul>
         </div>
     </nav>
-    
     <center>
-        <img src="sparx_bank__1__auto_x2_colored_toned_light_ai-removebg.png" class="img-fluid" alt="..." >
+  <img src="sparx_bank__1__auto_x2_colored_toned_light_ai-removebg.png" class="img-fluid" alt="..." >
+</center>
+<center>
+  <form action="details.php" method="post">
+      <input type="submit" value="get customer details">
+  </form>
+  <br>
+  <form action="transferslist.php" method="post">
+      <input type="submit" value="view previous transfers">
+  </form>
+</center>
 
-    </center>
-
-    <center>
-        <form action="details.php" method="post">
-            <input type="submit" value="get customer details">
-        </form>
-        <br>
-        <form action="transferslist.php" method="post">
-            <input type="submit" value="view previous transfers">
-        </form>
-    </center>
-
-   
-      <br>
-    
-    
-    
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
- 
-    
-      
-    
-    
-    
-    
-       
-
-</body>
-
-
+<br>
+    </body>
 
 </html>
