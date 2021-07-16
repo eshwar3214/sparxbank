b81b00cf5b29a7fe8280e7905b787099fb031f9d
Sai Eshwar Reddy Kottapally <76461247+eshwar3214@users.noreply.github.com>
2021-07-16 17:09:41 +0530

    added bootstrap to passbook

--- transferslist.php
+++ transferslist.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,58 +10,59 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>transfers list</title>
+    <title>passbook</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    
-    
-        <a class="navbar-brand" href="index.php">sparx bank</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="details.php">view customers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="transferslist.php">previous transfers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-        </ul>
-        </div>
-    </nav>
-    <h3 align='center'>previous transfers</h3>
+
+
   <?php
-    $username='sql5425083';
-    $password='x8ZZtmrind';
-    $server='sql5.freesqldatabase.com';
-    $dbname='sql5425083';
+
+$username='dWqRsnAtPe';
+$password='tOahk66aZU';
+$server='remotemysql.com';
+$dbname='dWqRsnAtPe';
+
+$con=new mysqli($server,$username,$password,$dbname);
+
+if($con->connect_error)
+{
+die("connection abborted due to ".mysqli_connect_error());
+
+}
+else
+{
+echo "server connection established";
+}
+
+$user=$_POST['name'];
+
+$sqa="SELECT * FROM details";
+
+$resulta=$con->query($sqa);
+
+while($rowa= $resulta->fetch_assoc()) {
+if($rowa['name']==$user)
+{
+    $balance=$rowa['balance'];
+    $email=$rowa['email'];
+    break;
+    
+}
 
 
-    $con=new mysqli($server,$username,$password,$dbname);
 
-    if($con->connect_error)
-    {
-        die("connection abborted due to ".mysqli_connect_error());
+}
 
-    }
-    else
-    {
-        echo "server connection established";
-    }
 
-    $sql = "SELECT `sno`,`transferredfrom`,`transferredto`,`transferredamount`,`senderbalance`,`recieverbalance`,`date`FROM `transfers`";
-    $result = $con->query($sql);
-    echo "<br>";
-    echo "
-       <div class='table-responsive'><table class='table table-bordered table-condensed'>
+$sqb="SELECT * FROM transfers";
+
+$resultb=$con->query($sqb);
+
+echo "<center>
+<br>$user $email $balance
+<h3>transactions</h3>";
+echo "
+    <table class='table'>
         <thead class='thead-dark'>
         <tr>
             <td>sno</td>
@@ -71,23 +74,25 @@
             <td>transfer date</td>
         </tr></thead><tbody>"
         ;
-        while($row = $result->fetch_assoc()) {
-            echo "<tr><td>".$row['sno']."</td><td>".$row['transferredfrom']."</td><td>".$row['transferredto']."</td><td>".$row['transferredamount']."</td><td>".$row['senderbalance']."</td><td>".$row['recieverbalance']."</td><td>".$row['date']."</td></tr>";
+    while($rowb = $resultb->fetch_assoc()) {
+        if($rowb['transferredfrom']==$user || $rowb['transferredto']==$user)
+        echo "<tr><td>".$rowb['sno']."</td><td>".$rowb['transferredfrom']."</td><td>".$rowb['transferredto']."</td><td>".$rowb['transferredamount']."</td><td>".$rowb['senderbalance']."</td><td>".$rowb['recieverbalance']."</td><td>".$rowb['date']."</td></tr>";
 
-            
-        }
-    echo "</tbody></table></div>";
+        
+    }
+    echo "</tbody></table>";
 
-    $con->close();
-    ?>
 
-    <center>
-    <form action="passbook.php" method="post">
-        <h3>get passbook of an account holder</h3>
-        <input type="text" name="name" placeholder><br><br>
-        <input type="submit" value="getpassbook">
-    </form>
-    </center>
+
+
+
+
+
+$con->close();
+
+
+?>
+  
 
     
 
