486887d1277a5d57cfd16ffbd12612c5aa16f3c7
Sai Eshwar Reddy Kottapally <76461247+eshwar3214@users.noreply.github.com>
2021-07-16 17:17:27 +0530

    added navbar

--- transferslist.php
+++ transferslist.php
@@ -11,6 +11,30 @@
     <title>transfers list</title>
   </head>
   <body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    
+    
+        <a class="navbar-brand" href="index.php">sparx bank</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="details.php">view customers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transferslist.php">previous transfers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </nav>
   <?php
     $username='sql5425083';
     $password='x8ZZtmrind';
