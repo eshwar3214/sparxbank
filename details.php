b05ddfe5f0da063fbaeb8403ec7274f62eeacbec
eshwar3214 <eshwar.smart2001@gmail.com>
2021-07-16 21:35:00 +0530

    changed navbar

--- details.php
+++ details.php
@@ -13,37 +13,36 @@
   
   <body>
     
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    
-    
-        <a class="navbar-brand" href="index.php">sparx bank</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">sparx bank</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
+      
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
                 <a class="nav-link" href="index.php">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="transfer.html">transfer</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="passbook.html">passbook</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="details.php">view customers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="transferslist.php">previous transfers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-        </ul>
-        </div>
-    </nav>
-    
+            <li class="nav-item active">
+              <a class="nav-link" href="transfer.html">transfer</a>
+          </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="passbook.html">passbook</a>
+          </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="details.php">view customers</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="transferslist.php">previous transfers</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+
+          </ul>
+          
+      </nav>
     
     <h3 align='center'>customer details</h1>
     <?php
@@ -120,6 +119,8 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-</body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
